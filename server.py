724016b36ee742a5d102f55c5a1ec7d799a1adc3
hjpotter92 <hjpotter92@iitkgp.ac.in>
2014-01-25 21:45:39 +0530

    temporary checkpoint

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-import socket, cPickle as pickle
+import socket, json, cPickle as pickle
 from player import Player
 
 class Server:
-	def __init__( self, host = "", port = 1992 ):
+	def __init__( self, host, port ):
 		self.socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 		self.socket.setblocking( False )
 		self.socket.bind( (host, port) )
+		# self.socket.listen( 4 )
+		print "Established connection on {} over {}".format( host, port )
 		self.list_users = {}
 
 	def receive( self ):
@@ -14,7 +16,7 @@ class Server:
 				data, addr = self.socket.recvfrom( 1024 )
 				pl = pickle.loads( data )
 				print '{} connected from {}'.format( pl.getNick(), addr[0] )
-				self.socket.sendto( pl.getNick(), addr )
+				self.socket.sendto( '{} users tracked.'.format(len( self.list_users )), addr )
 				if addr not in self.list_users:
 					self.list_users[addr] = data
 			except socket.error:
@@ -23,5 +25,9 @@ class Server:
 if __name__ == "__main__":
 	ip = raw_input( "Please enter the server IP address (this is shared with clients to connect): " )
 	port = raw_input( "Please enter the port to start server (leave blank for default): " )
+	if len( ip ) == 0 or len( port ) == 0:
+		with open( 'config/server.json') as config:
+			server_configuration = json.load( config )
+			ip, port = server_configuration['ip'], int( server_configuration['port'] )
 	srvr = Server( ip, int(port) )
 	srvr.receive()
