f574688637954d09e4a0413e8190acfcecf124c0
hjpotter92 <hjpotter92@iitkgp.ac.in>
2014-02-08 00:43:44 +0530

    Create input box class

---
+++ screen/input.py
@@ -0,0 +1,45 @@
+from pygame.locals import *
+from pygame import font as pyfont, event as pyevent, display as pydisplay
+from sys import exit
+
+class Box:
+	allowed_keys = range( K_0, K_9 + 1 ) + range( K_KP0, K_KP9 + 1 ) + [ K_PERIOD, K_KP_PERIOD ]
+	def __init__( self, screen, message, position, background = (255, 255, 255), text = (255, 0, 0) ):
+		pyfont.init()
+		self.screen = screen
+		self.message = message
+		self.position = position
+		self.colours = {
+			'screen': background,
+			'text': text
+		}
+		self.font = pyfont.Font( None, 36 )
+
+	def display( self, message ):
+		self.screen.fill( self.colours['screen'] )
+		if len( message ) > 0:
+			text = self.font.render( message, 1, self.colours['text'] )
+			self.screen.blit( text, self.position )
+		pydisplay.flip()
+
+	def run( self ):
+		return_string = ""
+		self.display( self.message )
+		running = True
+		while running:
+			for event in pyevent.get():
+				if event.type == QUIT:
+					exit( 0 )
+				elif event.type == KEYDOWN:
+					key_code, key_unicode = event.key, event.unicode
+					if key_code == K_ESCAPE:
+						exit( 0 )
+					if key_code == K_RETURN or key_code == K_KP_ENTER:
+						running = False
+						break
+					elif key_code == K_BACKSPACE:
+						return_string = return_string[:-1]
+					elif key_code in self.allowed_keys:
+						return_string += key_unicode
+				self.display( self.message + " " + return_string )
+		return return_string
