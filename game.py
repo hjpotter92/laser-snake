3b3262ae1802d6a10d9eb1fdc554ba3b8a36e650
hjpotter92 <hjpotter92+github@gmail.com>
2018-02-08 13:13:22 +0530

    Add `game.py` and `snake.py` missing from previous commit

---
+++ game.py
@@ -0,0 +1,108 @@
+from random import choice
+
+import pygame
+
+from pygame.locals import *
+
+from enums import Colours, Direction
+from snake import Snake
+from vector import Vector
+
+GAME_FPS = 30
+GAME_WINDOW = GAME_WIDTH, GAME_HEIGHT = 1280, 720
+
+BLOCK_SIZE = 8
+WORLD_SIZE = Vector((60, 60))
+
+KEY_DIRECTION = {
+    K_w: Direction.UP,
+    K_UP: Direction.UP,
+    K_a: Direction.LEFT,
+    K_LEFT: Direction.LEFT,
+    K_s: Direction.DOWN,
+    K_DOWN: Direction.DOWN,
+    K_d: Direction.RIGHT,
+    K_RIGHT: Direction.RIGHT,
+}
+
+
+class Game:
+    title = "Laser Snake"
+
+    def __init__(self):
+        self.block_size = BLOCK_SIZE
+        self.window = pygame.display.set_mode(GAME_WINDOW) #, pygame.FULLSCREEN)
+        self.screen = pygame.Surface(WORLD_SIZE * BLOCK_SIZE)
+        self.clock = pygame.time.Clock()
+        self.world = pygame.Rect((0, 0), WORLD_SIZE)
+        pygame.display.set_caption(self.title)
+        pygame.mouse.set_visible(False)
+        self.reset()
+
+    def reset(self):
+        self.running = True
+        self.next_direction = Direction.RANDOM()
+        self.snake = Snake(
+            Vector(self.world.center) // 2,
+            self.next_direction.value,
+            length=5,
+            color=Colours.RANDOM()
+        )
+
+    def check_exit(self, event):
+        """Check if the user exited"""
+        if (event.type == pygame.QUIT or
+                (event.type == KEYDOWN and event.key == K_ESCAPE)):
+            self.running = False
+            return True
+
+    def key_input(self, event):
+        if event.key in KEY_DIRECTION:
+            self.next_direction = KEY_DIRECTION[event.key]
+
+    def cleanup(self):
+        pygame.quit()
+
+    def block(self, position):
+        return pygame.Rect(
+            position * self.block_size,
+            Direction.BOTTOM_RIGHT.value * self.block_size
+        )
+
+    def update(self, δt):
+        self.snake.update(δt, self.next_direction.value)
+        if self.snake.suicide():
+            self.running = False
+        if not self.world.collidepoint(self.snake.head()):
+            self.snake.loop(self.world.topleft, self.world.bottomright)
+
+    def render(self):
+        # self.window.fill(Colours.BACKGROUND.value)
+        self.screen.fill(Colours.BACKGROUND.value)
+        for segment in self.snake:
+            pygame.draw.ellipse(
+                self.screen,
+                self.snake.get_color(),
+                self.block(segment)
+            )
+        self.window.blit(self.screen, self.window.get_rect(x=80, y=40))
+
+    def run(self):
+        while self.running:
+            δt = self.clock.tick(GAME_FPS) / 1000.0
+            self.render()
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if self.check_exit(event):
+                    break
+                if event.type == KEYUP:
+                    self.key_input(event)
+            self.update(δt)
+            pygame.display.flip()
+        self.cleanup()
+
+
+if __name__ == "__main__":
+    pygame.init()
+    game = Game()
+    game.run()
---
+++ snake.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+from enums import Direction
+from vector import Vector
+
+SNAKE_MIN_SPEED = 8.0
+SNAKE_MIN_LENGTH = 5
+
+
+class Snake:
+    def __init__(self, start, direction, color=None, length=1, speed=1):
+        self.__direction = direction
+        self.length = max(SNAKE_MIN_LENGTH, length)
+        self.speed = max(SNAKE_MIN_SPEED, speed)
+        self.segments = deque([start])
+        self.color = color
+        self.set_timer()
+        print(self.segments)
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __len__(self):
+        return len(self.segments)
+
+    def __iter__(self):
+        return iter(self.segments)
+
+    def head(self):
+        return self.segments[0]
+
+    def extend(self, segment=None):
+        self.segments.appendleft((segment or self.head()) + self.get_direction())
+
+    def suicide(self):
+        return self.segments.count(self.head()) > 1
+
+    def grow(self):
+        self.length += 1
+
+    def loop(self, topleft, bottomright):
+        head = self.head()
+        if self.get_direction() == Direction.RIGHT.value:
+            new_vector = Vector((topleft[0], head[1]))
+        elif self.get_direction() == Direction.UP.value:
+            new_vector = Vector((head[0], bottomright[1]))
+        elif self.get_direction() == Direction.LEFT.value:
+            new_vector = Vector((bottomright[0], head[1]))
+        elif self.get_direction() == Direction.DOWN.value:
+            new_vector = Vector((head[0], topleft[1]))
+        self.extend(new_vector)
+
+    def get_color(self):
+        return self.color.value
+
+    def get_direction(self):
+        return self.__direction
+
+    def set_direction(self, direction):
+        if self.__direction != -direction:
+            self.__direction = direction
+
+    def set_timer(self):
+        self.__timer = 1.0 / self.speed
+
+    def update(self, δt, direction):
+        self.__timer -= δt
+        if self.__timer > 0:
+            return
+        self.set_timer()
+        self.set_direction(direction)
+        self.extend()
+        if len(self) > self.length:
+            self.segments.pop()
