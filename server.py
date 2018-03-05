c52d618559d6624cc04aaa605cd38cd3427e3c79
hjpotter92 <hjpotter92+github@gmail.com>
2018-03-05 22:03:06 +0530

    Enhanced protocol for serverside handling of games
     * Define `@property` for several values in `Game` and `Player` objects
     * Check player moving out of world bounds
     * Helpers to randomly place snakes and food items on game board
     * Keep a ticker in server for flooding data to clients
    
    TODO: Keep track of disconnected users

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import asyncio
 
 from collections import deque
+from math import log10
+from random import choice, randint
 from uuid import uuid4
 
-from rencode import loads, dumps
+from rencode import dumps
 
 from enums import Direction
 from food import Food
@@ -17,13 +19,15 @@ GAME_TICKER = 1 / GAME_FPS
 
 class Player:
     """docstring for Player"""
-    def __init__(self, game_id, id_, address):
+    def __init__(self, game_id, id_, address, **kwds):
         self._id = id_
         self._address = address
         self._game_id = game_id
         self._joined = TIME()
         self._connected = True
         self._alive = True
+        self.boundary = kwds.get('boundary')
+        self.next_direction = Direction.RANDOM()
         self.snake = None
 
     @property
@@ -36,7 +40,48 @@ class Player:
 
     @property
     def dead(self):
-        return not self._alive
+        return not self.alive
+
+    @property
+    def alive(self):
+        return self._alive
+
+    @property
+    def topleft(self):
+        return self.boundary[0]
+
+    @property
+    def bottomright(self):
+        return self.boundary[1]
+
+    @property
+    def direction(self):
+        if self.snake is None:
+            return self.next_direction.value
+        return self.snake.direction
+
+    @direction.setter
+    def direction(self, value):
+        if not isinstance(value, Direction):
+            raise TypeError("enum expected")
+        self.next_direction = value
+
+    def snake_packet(self):
+        return {
+            'head': tuple(self.snake.head),
+            'segments': self.snake.blocks,
+            'direction': Direction(self.direction).name,
+            'player': {
+                'id': self.id,
+                'alive': not self.dead
+            }
+        }
+
+    def inside_boundary(self, point):
+        return (
+            self.topleft[0] <= point[0] <= self.bottomright[0] and
+            self.topleft[1] <= point[1] <= self.bottomright[1]
+        )
 
     def set_game(self, game_id):
         self._game_id = game_id
@@ -44,10 +89,17 @@ class Player:
     def attach_snake(self, snake):
         self.snake = snake
 
+    def check_bounds(self):
+        if not self.inside_boundary(self.snake.head):
+            self.snake.loop(*self.boundary)
+
+    def update(self, δt):
+        self.snake.update(δt, self.next_direction.value)
+
 
 class Game:
     """docstring for Game"""
-    def __init__(self, id_, transport, loop, *, max_players=2, world_size=None):
+    def __init__(self, id_, transport, loop, max_players=2, world_size=None):
         self._id = id_
         self._transport = transport
         self._running = False
@@ -66,8 +118,33 @@ class Game:
     def max_players(self):
         return self._max_players
 
+    @property
+    def boundary(self):
+        return (
+            (0, 0),
+            self.world_size - Direction.BOTTOM_RIGHT.value
+        )
+
     def __len__(self):
         return len(self.players)
+
+    def get_player(self, player_id):
+        player = self.players[player_id]
+        if player.id == player_id:
+            return player
+        return next(
+            (player for player in self.players if player.id == player_id)
+        )
+
+    def get_tick(self):
+        last_tick = self.loop.time()
+
+        def wrapped():
+            nonlocal last_tick
+            δt = self.loop.time() - last_tick
+            last_tick = self.loop.time()
+            return δt
+        return wrapped
 
     def is_running(self):
         return self._running
@@ -95,39 +172,70 @@ class Game:
         }
         self.broadcast(error_data, except_player_id)
 
+    def generate_snake(self, player):
+        snakes = [
+            p.snake for p in self.players
+            if p != player and p.snake is not None
+        ]
+        exceptions = []
+        for snake in snakes:
+            exceptions += snake.blocks
+        pos = random_vector(*self.boundary, exceptions=exceptions)
+        player.snake = snake = Snake(pos, player.direction)
+        return snake
+
+    def generate_food(self):
+        exceptions = []
+        for player in self.players:
+            exceptions += player.snake.blocks
+        for food in self.foods:
+            exceptions += food.position
+        position = random_vector(
+            *self.boundary,
+            exceptions=exceptions
+        )
+        rand = randint(2, 9999)
+        score = int(3 / log10(rand)) + 1
+        self.foods.add(Food(position, score))
+
     def add_player(self, *args):
-        player = Player(self.id, len(self), *args)
+        player = Player(self.id, len(self), *args, boundary=self.boundary)
+        snake = self.generate_snake(player)
         self.players.append(player)
         return player
 
     def start_game(self):
         start_data = {
             'action': 'start',
-            'world_size': self.world_size
+            'world_size': self.world_size,
+            'snakes': [player.snake_packet() for player in self.players]
         }
+        self.generate_food()
         self._running = True
         self.broadcast(start_data)
         self.loop.create_task(self.run(GAME_TICKER))
 
-    def get_tick(self):
-        last_tick = self.loop.time()
-
-        def wrapped():
-            nonlocal last_tick
-            δt = self.loop.time() - last_tick
-            last_tick = self.loop.time()
-            return δt
-        return wrapped
+    def update(self, δt):
+        for player in self.players:
+            player.update(δt)
+            player.check_bounds()
+        self.broadcast({
+            'action': 'objects',
+            'snakes': [player.snake_packet() for player in self.players],
+            'foods': [food.packet() for food in self.foods]
+        })
 
     async def run(self, ticker):
+        await asyncio.sleep(1)
         while self.is_running():
-            ticker = await asyncio.sleep(ticker, result=self.get_tick())
-            δt = ticker()
-            print(δt)
-            self.broadcast_error("successful ticker implemented")
+            tick = await asyncio.sleep(ticker, result=self.get_tick())
+            δt = tick()
+            self.update(δt)
 
     def process_action(self, action, data):
-        print(action)
+        if action == "move":
+            player = self.get_player(data.get('player_id'))
+            player.direction = Direction[data.get('direction').upper()]
 
 
 class Server(asyncio.DatagramProtocol):
@@ -155,16 +263,18 @@ class Server(asyncio.DatagramProtocol):
         self._transport.sendto(dumps(error_data), addr)
 
     def join_game(self, data, addr):
-        game = self.get_game()
+        game = self.get_game(game_id=data.get('game_id'))
         player = game.add_player(addr)
-        game.send_message(player, {'action': 'joined'})
+        game.send_message(
+            player,
+            {'action': 'joined', 'snake': player.snake_packet()}
+        )
         if game.can_start():
             self.queue = None
             game.start_game()
 
     def datagram_received(self, data, addr):
         data = parse_packet(data)
-        print(data)
         action = data.get('action')
         if action == 'join':
             self.join_game(data, addr)
