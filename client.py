1eb26a64f556e9e071b29f4f58c9908df8d62b43
hjpotter92 <hjpotter92+github@gmail.com>
2018-03-06 06:27:40 +0530

    Client side protocol for handling server packets
    Execute game in a separate `asyncio.Executor`

--- client.py
+++ client.py
@@ -3,14 +3,33 @@ import logging
 
 from rencode import loads, dumps
 
+from enums import Direction
+from game import SnakeGame
+from utils import TIME, parse_packet
+
+FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('laser-snake.client')
 logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.FileHandler('client.log'))
 
 
 class Client(asyncio.DatagramProtocol):
     """docstring for Client"""
     def __init__(self):
         self._transport = None
+        self._game_id = None
+        self._player_id = None
+        self.started = False
+        self.game = None
+
+    @property
+    def game_id(self):
+        return self._game_id
+
+    @property
+    def player_id(self):
+        return self._player_id
 
     def connection_made(self, transport):
         self._transport = transport
@@ -19,9 +38,7 @@ class Client(asyncio.DatagramProtocol):
             'x': 123,
             'y': list(range(5))
         }
-        self._transport.sendto(
-            dumps(req)
-        )
+        self.send_message(req)
 
     def connection_lost(self, exc):
         loop = asyncio.get_event_loop()
@@ -31,16 +48,32 @@ class Client(asyncio.DatagramProtocol):
     def error_received(self, exc):
         raise exc
 
-    def parse_packet(self, data):
-        data = loads(data)
-        return {
-            k.decode(): v.decode() if isinstance(v, bytes) else v
-            for k, v in data.items()
-        }
+    def send_message(self, message):
+        if not isinstance(message, dict):
+            return
+        message['game_id'] = self.game_id
+        message['player_id'] = self.player_id
+        self._transport.sendto(dumps(message))
+
+    def set_game(self, data):
+        self.game = SnakeGame(client=self, **data)
+        self._game_id = data.get('game_id')
+        self._player_id = data.get('player_id')
+        logger.debug(data)
+
+    def start_game(self, data):
+        def executor():
+            game = self.game
+            game.run(**data)
+            game.quit()
+        if self.started is True:
+            return
+        loop = asyncio.get_event_loop()
+        loop.run_in_executor(None, executor)
+        self.started = True
 
     def datagram_received(self, data, addr):
-        data = self.parse_packet(data)
-        logger.info(data)
+        data = parse_packet(data)
         action = data.get('action')
         if action is None:
             return
@@ -48,10 +81,26 @@ class Client(asyncio.DatagramProtocol):
             logger.error(data.get('error'))
             return
         game_id = data.get('game_id')
+        player_id = data.get('player_id')
+        if action == 'joined':
+            self.set_game(data)
+            return
+        if self.game_id != game_id:
+            logger.error("Wrong game_id detected")
+            return
+        if action == 'start' and self.started is False:
+            logger.info(f"Starting game at {TIME()}")
+            self.start_game(data)
+            return
+        if action == 'objects':
+            self.game.parse_snakes(data.get('snakes'))
+            self.game.foods = data.get('foods')
+        elif action == 'over':
+            logger.debug(data)
+            self.game.over()
 
 
 if __name__ == "__main__":
-    print('hey')
     loop = asyncio.get_event_loop()
     listener = loop.create_datagram_endpoint(
         Client,
