78873c22b27b19700532e0ac240c0a407a64959d
hjpotter92 <hjpotter92+github@gmail.com>
2018-03-06 06:25:25 +0530

    Implement logging module for clients to debug game when crashing
     * Provide layer for `client.py` to interact when datagram is received
      from server
     * Remove logical computation from client side code
     * Fix snake and food colours
     * Varied width of food item depending on its score value

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import logging
+
 from math import log10
 from random import choice, randint
 
@@ -11,7 +13,7 @@ from food import Food
 from snake import Snake
 from vector import Vector, random_vector
 
-GAME_FPS = 30
+GAME_FPS = 60
 GAME_WINDOW = GAME_WIDTH, GAME_HEIGHT = 1280, 720
 GAME_FULLSCREEN = False
 
@@ -28,62 +30,52 @@ KEY_DIRECTION = {
     K_RIGHT: Direction.RIGHT,
 }
 
+FORMAT = '%(asctime)-15s [%(levelname)s] (%(name)s) %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('laser-snake.client.game')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.FileHandler('client.log'))
+
 
 class SnakeGame:
     """
     The layer for handling the main game window and its individual elements.
 
     """
-    title = "Laser Snake"
+    title = "Laser Snake - "
 
-    def __init__(self, *, world_size=None):
+    def __init__(self, game_id=None, *args, **kwds):
+        self.client = kwds.get('client')
+        self.game_id = game_id
+        self.snake = kwds.get('snake')
+        self.player_id = kwds.get('player_id')
+        self.running = False
+        self.foods = None
+
+    def display(self, world_size):
         pygame.init()
         world_size = Vector(world_size) if world_size else WORLD_SIZE
         self.block_size = 480 // world_size[0]
         self.window = pygame.display.set_mode(GAME_WINDOW, GAME_FULLSCREEN)
         self.screen = pygame.Surface(world_size * self.block_size)
         self.clock = pygame.time.Clock()
-        self.world = pygame.Rect((0, 0), WORLD_SIZE)
-        self.boundary = (
-            self.world.topleft,
-            self.world.bottomright - Direction.BOTTOM_RIGHT.value
-        )
+        self.world = pygame.Rect((0, 0), world_size)
         self.font = pygame.font.Font(
             pygame.font.match_font('helvetica,tahoma,arial'),
             32
         )
-        pygame.display.set_caption(self.title)
-        pygame.mouse.set_visible(False)
-        self.reset()
-
-    def reset(self):
+        pygame.display.set_caption(f"{self.title}{self.game_id}")
         self.running = True
-        self.next_direction = Direction.RANDOM()
-        starting = random_vector(*self.boundary)
-        self.snake = Snake(
-            starting,
-            self.next_direction.value,
-            color=Colors.BLUE
-        )
-        self.snakes = [
-            Snake(
-                random_vector(*self.boundary),
-                Direction.RANDOM().value,
-                color=Colors.RED
-            )
-            for i in range(3)
-        ]
-        self.foods = set()
-        self.generate_food()
 
-    def generate_food(self):
-        position = random_vector(
-            *self.boundary,
-            exceptions=self.snake
-        )
-        rand = randint(2, 9999)
-        score = int(3 / log10(rand)) + 1
-        self.foods.add(Food(position, score, Colors.GREEN))
+    def parse_snakes(self, snakes):
+        # logger.critical(snakes)
+        self.others = []
+        for snake in snakes:
+            player = snake.get('player')
+            if player.get('id') == self.player_id:
+                self.snake = snake
+            else:
+                self.others.append(snake)
 
     def check_exit(self, event):
         """Check if the user exited"""
@@ -95,18 +87,18 @@ class SnakeGame:
     def key_input(self, event):
         if event.key in KEY_DIRECTION:
             self.next_direction = KEY_DIRECTION[event.key]
-        elif event.key in (K_SPACE, K_r):
-            self.reset()
+            self.client.send_message({
+                'action': 'move',
+                'head': self.snake.get('head'),
+                'direction': self.next_direction.name
+            })
 
     def cleanup(self):
         pygame.quit()
 
-    def quit(self):
-        pygame.quit()
-
     def block(self, position):
         return pygame.Rect(
-            position * self.block_size,
+            Vector(position) * self.block_size,
             Direction.BOTTOM_RIGHT.value * self.block_size
         )
 
@@ -117,64 +109,50 @@ class SnakeGame:
         )
 
     def draw_snake(self, snake):
-        snake_head = snake.head()
-        for segment in snake:
-            if snake_head is segment:
-                pyellipse(
-                    self.screen,
-                    snake.get_color(),
-                    self.block(segment)
-                )
-            else:
-                pyarc(
-                    self.screen,
-                    snake.get_color(),
-                    self.block(segment), 0, 360,
-                    self.block_size//4
-                )
+        head = snake.get('head')
+        player = snake.get('player')
+        color = Colors['BLUE' if player.get('id') == self.player_id else 'RED']
+        pyellipse(
+            self.screen,
+            color.value,
+            self.block(head)
+        )
+        for segment in snake.get('segments'):
+            pyarc(
+                self.screen,
+                color.value,
+                self.block(segment), 0, 360,
+                self.block_size//4
+            )
+
+    def over(self):
+        pygame.display.set_caption(f"{self.title}{self.game_id} - Game Over")
 
     def render(self):
         self.window.fill(Colors.BACKGROUND.value)
         self.screen.fill(Colors.WHITE.value)
         self.draw_snake(self.snake)
-        for snake in self.snakes:
+        for snake in self.others:
             self.draw_snake(snake)
         for food in self.foods:
             pyrect(
                 self.screen,
-                food.get_color(),
-                self.block(food.get_position())
+                Colors.GREEN.value,
+                self.block(food.get('position')),
+                self.block_size // (4 * food.get('score'))
             )
         self.window.blit(self.screen, self.window.get_rect(x=80, y=40))
         self.draw_text(
-            "Score: {0:n}. Speed: {1:02.2f}".format(
-                self.snake.get_score(),
-                self.snake.get_speed()
+            "Score: {0:n}".format(
+                len(self.snake.get('segments'))
             ),
             (80, 600)
         )
 
-    def update(self, δt):
-        self.snake.update(δt, self.next_direction.value)
-        for snake in self.snakes:
-            snake.update(δt, Direction.RANDOM().value)
-        head = self.snake.head()
-        for food in self.foods:
-            if food.get_position() == head:
-                self.foods.remove(food)
-                self.snake.grow(food.get_score())
-                self.generate_food()
-        if self.snake.suicide():
-            self.next_direction = Direction.NULL
-            self.running = False
-        if not self.world.collidepoint(head):
-            self.snake.loop(*self.boundary)
-        for snake in self.snakes:
-            head = snake.head()
-            if not self.world.collidepoint(head):
-                snake.loop(*self.boundary)
-
-    def run(self):
+    def run(self, **kwds):
+        self.display(kwds.get('world_size'))
+        self.parse_snakes(kwds.get('snakes'))
+        logger.info("display started")
         while True:
             δt = self.clock.tick(GAME_FPS) / 1000.0
             pygame.event.pump()
@@ -184,10 +162,11 @@ class SnakeGame:
                 if event.type == KEYUP:
                     self.key_input(event)
             if self.running is True:
-                self.update(δt)
                 self.render()
             pygame.display.flip()
         self.cleanup()
+
+    quit = cleanup
 
 
 if __name__ == "__main__":
